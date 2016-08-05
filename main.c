94a90847f8f1892ea1cc3853d400150752d0f5e5
omarsolares <wicho.omar@gmail.com>
2016-08-04 23:23:55 -0600

    automata

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void automata(char inst[80]){
+    const char s[10]=" ::-/\"\\";
+    char *token;
+
+    token = strtok(inst,s);
+    while(token!=NULL){
+        printf( " %s\n", token );
+        token = strtok(NULL, s);
+    }
+}
 
 int main()
 {
+    int i =1;
+    char instruccion[80];
+    do{
+            printf("root@omar_201403981:~$ ");
+            scanf("%s",&instruccion);
+            //automata(instruccion);
 
-    printf("Hello world!\n");
+    }while(strcmp(instruccion,"exit")!=0);
 
     return 0;
 }
