11bba95b4c3fd3387863731f826533fa3081bff3
omarsolares <wicho.omar@gmail.com>
2016-08-08 22:09:29 -0600

    capturar valores de la entrada

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 
 void automata(char inst[80]){
     int size=0;
-    char unit='M';
+    char unit[5];
+    strcpy(unit,"M");
     char nombre[20];
     char path[50];
     const char s[20]=" ::-\"\\+";
@@ -14,7 +15,7 @@ void automata(char inst[80]){
     int estado=0;
     token = strtok(inst,s);
     while(token!=NULL){
-
+        //printf("%d",estado);
        // printf("%s",token);
         switch(estado){
             case 0:
@@ -28,15 +29,15 @@ void automata(char inst[80]){
                     printf("En size\n");
                     estado=2;
                 }
-                if(strcmp(token,"name")==0){
+                else if(strcmp(token,"name")==0){
                     printf("En name\n");
                     estado=3;
                 }
-                if(strcmp(token,"path")==0){
+                else if(strcmp(token,"path")==0){
                     printf("En path\n");
                     estado=4;
                 }
-                if(strcmp(token,"unit")==0){
+                else if(strcmp(token,"unit")==0){
                     printf("En unit\n");
                     estado=5;
                 }else{
@@ -45,36 +46,30 @@ void automata(char inst[80]){
                 break;
             case 2:
                 printf("En valor de size: %s\n",token);
+                size=(int)token;
                 estado =1;
                 break;
             case 3:
                 printf("En valor de name: %s\n",token);
+                strcpy(nombre,token);
                 estado =1;
                 break;
             case 4:
                 printf("En valor de path: %s\n",token);
+                strcpy(path,token);
                 estado =1;
                 break;
             case 5:
                 printf("En valor de unit: %s\n",token);
+                strcpy(unit,token);
                 estado =1;
                 break;
         }
 
         token = strtok(NULL, s);
     }
+    printf("size: %d, name: %s, path: %s, unit: %s\n",size,nombre,path,unit);
 }
-
-/*void crearDisco(char comando[80]){
-    int size=0;
-    char unit='M';
-    char nombre[20];
-    char path[50];
-    const char s[20]=" ::-\"\\+";
-    char *token;
-    int estado=0;
-    token = strtok(inst,s);
-}*/
 
 int main()
 {
