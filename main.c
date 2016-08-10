7fdd95aeba90fbc999031b1e7c66f55dc230b233
omarsolares <wicho.omar@gmail.com>
2016-08-09 22:11:48 -0600

    comando mkdisk

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
+#include <stdlib.h>
+#include <time.h>
+typedef struct particion{
+    char part_status;
+    char part_type;
+    char part_fit;
+    int part_start;
+    int part_size;
+    char part_name[16];
+};
+struct mbr{
+        int mbr_tamano;
+        char mbr_fecha_creacion[128];
+        int mbr_disk_signature;
+        struct particion *mbr_partition_1;
+        struct particion *mbr_partition_2;
+        struct particion *mbr_partition_3;
+        struct particion *mbr_partition_4;
+};
 
 void automata(char inst[80]){
     int size=0;
@@ -41,12 +58,14 @@ void automata(char inst[80]){
                     printf("En unit\n");
                     estado=5;
                 }else{
+                    crearDisco(size,path,nombre,unit);
+                    leer(path,nombre);
                     estado=0;
                 }
                 break;
             case 2:
                 printf("En valor de size: %s\n",token);
-                size=(int)token;
+                size=atoi(token);
                 estado =1;
                 break;
             case 3:
@@ -71,8 +90,59 @@ void automata(char inst[80]){
     printf("size: %d, name: %s, path: %s, unit: %s\n",size,nombre,path,unit);
 }
 
+void crearDisco(int size, char path[50], char nombre[50], char unit[5]){
+    srand(time(NULL));
+
+    time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+    char output[128];
+    strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal);
+
+    struct mbr nuevoDisco;
+    nuevoDisco.mbr_tamano=size;
+    strcpy(nuevoDisco.mbr_fecha_creacion,output);
+    nuevoDisco.mbr_disk_signature=10+(rand()%991);
+    nuevoDisco.mbr_partition_1=NULL;
+    nuevoDisco.mbr_partition_2=NULL;
+    nuevoDisco.mbr_partition_3=NULL;
+    nuevoDisco.mbr_partition_4=NULL;
+    FILE *file;
+    char direccion[80];
+    strcpy(direccion,path);
+    strcat(direccion,nombre);
+
+    file = fopen(direccion,"w");
+    //fseek(file,sizeof(mbr),SEEK_SET);
+    fwrite(&nuevoDisco,sizeof(struct mbr),1,file);
+    fclose(file);
+}
+void leer(char path[50], char nombre[50]){
+
+    struct mbr a;
+    char direccion[80];
+    strcpy(direccion,path);
+    strcat(direccion,nombre);
+    FILE *file;
+    file = fopen(direccion,"r");
+
+    fread(&a,sizeof(struct mbr),1,file);
+
+    printf("Fecha de creacion: %s\n",a.mbr_fecha_creacion);
+    fclose(file);
+}
+
 int main()
 {
+    time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+    char output[128];
+    strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal);
+    printf("%s\n",output);
+    char a[50];
+    char b[50];
+    strcpy(a,"/home/omar/Escritorio/");
+    strcpy(b,"pruba.dsk");
+    leer(a,b);
     int i =1;
     char instruccion[80];
     do{
